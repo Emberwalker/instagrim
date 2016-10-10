f48230db9448ba60ab3825227f6c48544b49293e
Arkan Emberwalker <arkan@drakon.io>
2016-10-10 12:42:51 +0100

    Further minimise junk in PicModel

--- src/main/kotlin/io/drakon/uni/ac32007/instagrim/models/PicModel.kt
+++ src/main/kotlin/io/drakon/uni/ac32007/instagrim/models/PicModel.kt
@@ -4,20 +4,13 @@ import com.datastax.driver.core.BoundStatement
 import com.datastax.driver.core.Cluster
 import com.datastax.driver.core.PreparedStatement
 import com.datastax.driver.core.ResultSet
-import io.drakon.uni.ac32007.instagrim.img.DecolourImg
-import io.drakon.uni.ac32007.instagrim.img.ThumbImg
 
-import java.awt.image.BufferedImage
-import java.io.ByteArrayOutputStream
-import java.io.File
-
-import java.io.IOException
 import java.nio.ByteBuffer
 import java.util.Date
-import javax.imageio.ImageIO
-import org.imgscalr.Scalr.*
 import org.slf4j.LoggerFactory
 
+import io.drakon.uni.ac32007.instagrim.img.DecolourImg
+import io.drakon.uni.ac32007.instagrim.img.ThumbImg
 import io.drakon.uni.ac32007.instagrim.lib.Convertors
 import io.drakon.uni.ac32007.instagrim.stores.Pic
 
@@ -31,33 +24,26 @@ class PicModel {
     }
 
     fun insertPic(b: ByteArray, type: String, name: String, user: String) {
-        try {
+        val types = Convertors.SplitFiletype(type)
+        val buffer = ByteBuffer.wrap(b)
+        val length = b.size
+        val picid = Convertors.timeUUID
 
-            val types = Convertors.SplitFiletype(type)
-            val buffer = ByteBuffer.wrap(b)
-            val length = b.size
-            val picid = Convertors.timeUUID
+        val thumbbuf = ThumbImg.process(b, types[1])
+        val thumblength = thumbbuf.size
+        val processedbuf = DecolourImg.process(b, types[1])
+        val processedlength = processedbuf.size
 
-            val thumbbuf = ThumbImg.process(b, types[1])
-            val thumblength = thumbbuf.size
-            val processedbuf = DecolourImg.process(b, types[1])
-            val processedlength = processedbuf.size
+        val session = cluster.connect("instagrim")
+        val psInsertPic = session.prepare("insert into pics ( picid, image,thumb,processed, user, interaction_time,imagelength,thumblength,processedlength,type,name) values(?,?,?,?,?,?,?,?,?,?,?)")
+        val psInsertPicToUser = session.prepare("insert into userpiclist ( picid, user, pic_added) values(?,?,?)")
+        val bsInsertPic = BoundStatement(psInsertPic)
+        val bsInsertPicToUser = BoundStatement(psInsertPicToUser)
 
-            val session = cluster.connect("instagrim")
-            val psInsertPic = session.prepare("insert into pics ( picid, image,thumb,processed, user, interaction_time,imagelength,thumblength,processedlength,type,name) values(?,?,?,?,?,?,?,?,?,?,?)")
-            val psInsertPicToUser = session.prepare("insert into userpiclist ( picid, user, pic_added) values(?,?,?)")
-            val bsInsertPic = BoundStatement(psInsertPic)
-            val bsInsertPicToUser = BoundStatement(psInsertPicToUser)
-
-            val DateAdded = Date()
-            session.execute(bsInsertPic.bind(picid, buffer, thumbbuf, processedbuf, user, DateAdded, length, thumblength, processedlength, type, name))
-            session.execute(bsInsertPicToUser.bind(picid, user, DateAdded))
-            session.close()
-
-        } catch (ex: IOException) {
-            log.error("I/O Error during insertPic", ex)
-        }
-
+        val DateAdded = Date()
+        session.execute(bsInsertPic.bind(picid, buffer, thumbbuf, processedbuf, user, DateAdded, length, thumblength, processedlength, type, name))
+        session.execute(bsInsertPicToUser.bind(picid, user, DateAdded))
+        session.close()
     }
 
     fun getPicsForUser(User: String): java.util.LinkedList<Pic>? {
